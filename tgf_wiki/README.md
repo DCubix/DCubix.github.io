fac65f6179598c3a2c5deaecb20090de9ecd9050
DCubix <diego95lopes@live.com>
2021-03-20 17:42:04 -0400

    New portfolio look

--- tgf_wiki/README.md
+++
@@ -1,5 +0,0 @@
-# Wiktor
-A minimalist wiki engine
-
-
-This is an in progress wiki engine, it may or may not contain bees.
--- tgf_wiki/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find entries/* \
-| sed 's/entries\///g' \
-| awk '{ print "\""$0"\""}' \
-| paste -sd "," - \
-| awk '{ print "[" $0 "]" }' \
-> wiktor/entries.json
