51c6b618f8bdb85466c7490f56717336ad6e1f89
DCubix <diego95lopes@live.com>
2023-01-17 13:16:56 -0400

    :construction: GH link + Video support

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import ProfilePicture from "./lib/components/ProfilePicture.svelte";
-  import { Github, Linkedin, Mail } from 'lucide-svelte';
+  import { Github, Linkedin, Mail, Play, PlayCircle } from 'lucide-svelte';
   import data from '../public/data.json';
+
+  data.reverse();
 </script>
 
 <main class="w-full md:w-[90%] lg:w-3/4 xl:w-2/3 2xl:w-5/12 m-auto p-3 py-5 md:p-12">
@@ -36,18 +38,49 @@
     {#each data as item}
       <div class="bg-black bg-opacity-30 p-5 rounded-3xl my-2">
         <div class="flex flex-col">
-          <span class="font-semibold text-xl">{item.name}</span>
+          <div class="flex justify-end">
+            <span class="font-semibold text-xl">{item.name}</span>
+            <div class="flex-auto"></div>
+            
+            {#if item.github}
+              <a class="flatbutton" href={item.github}>
+                <Github size={16} />&nbsp;GitHub
+              </a>
+            {/if}
+
+            {#if item.demo}
+              <a class="flatbutton" href={item.demo}>
+                <Play size={16} />&nbsp;Demo
+              </a>
+            {/if}
+          </div>
           <span class="text-white text-opacity-70 mb-3">{item.description}</span>
         </div>
         <div class="grid-tiles">
           {#each item.images as image}
-            <a
-              href={image}
-              rel="noreferrer"
-              target="_blank"
-              class="grid-tile bg-center bg-cover shadow-2xl"
-              style={"background-image: url('" + image + "')"}>
-            </a>
+            {#if ['mp4','flv','ogg','avi'].includes(image.substring(image.lastIndexOf('.')+1))}
+              <!-- video -->
+              <!-- svelte-ignore a11y-media-has-caption -->
+              <a
+                href={image}
+                rel="noreferrer"
+                target="_blank"
+                class="grid-tile bg-center bg-cover shadow-2xl overflow-hidden relative">
+                <video preload="metadata" muted class="w-full h-full object-cover z-0">
+                  <source src={image + '#t=15'} type={"video/" + image.substring(image.lastIndexOf('.')+1)}>
+                </video>
+                <PlayCircle size={60} class="z-10 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2" />
+              </a>
+            {:else}
+              <!-- image -->
+              <a
+                href={image}
+                rel="noreferrer"
+                target="_blank"
+                class="grid-tile bg-center bg-cover shadow-2xl"
+                style={"background-image: url('" + image + "')"}>
+              </a>
+            {/if}
           {/each}
         </div>
       </div>
